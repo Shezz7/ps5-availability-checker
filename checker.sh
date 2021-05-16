b37e76606c609de6bf0d06779cd149cf03cbf3b2
Shezz7 <shehzaad.saifulla@tink.se>
2021-05-16 08:07:50 +0200

    feat(ps5checker): check amazon de

--- checker.sh
+++ checker.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
 # ps5 digital edition
-amazonukdig='https://www.amazon.co.uk/dp/B08H97NYGP'
+amazondig='B08H97NYGP'
 
 # ps5 standard edition
-amazonukstd='https://www.amazon.co.uk/dp/B08H95Y452'
+amazonstd='B08H95Y452'
 
 check_amazon() {
     versions="$@"
+    regions="co.uk de"
 
     for version in $versions; do
-        status=$(curl -s "$version" -H 'User-Agent: Mozilla/5.0 (X11; Linux i686; rv:88.0) Gecko/20100101 Firefox/88.0' \
-        | grep -i "div id=\"availability\"" -A 10 | sed -n '7 p')
-
-        if [ "$status" != "Currently unavailable." ]; then
-            echo "AVAILABLE!"
-        else
-            echo "$(date):out of stock"
-        fi
+        for region in $regions; do
+            echo $region
+            status=$(curl -s https://www.amazon.$region/dp/$version -H 'User-Agent: Mozilla/5.0 (X11; Linux i686; rv:88.0) Gecko/20100101 Firefox/88.0' \
+            | grep -i "div id=\"availability\"" -A 10 | sed -n '7 p')
+            echo $status
+            if [ "$status" != "Currently unavailable." ]; then
+                echo "AVAILABLE!"
+            else
+                echo "$(date):Amazon is out of stock"
+            fi
+        done
     done
 }
 
-check_amazon "$amazonukdig $amazonukstd"
+check_amazon "$amazondig $amazonstd"
