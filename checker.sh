0cd2d62cd73ee233e4dc1c6e64899d316be66814
Shezz7 <shehzaad.saifulla@tink.se>
2021-05-17 07:38:23 +0200

    fix(ps5checker): fix timezone in logs

--- checker.sh
+++ checker.sh
@@ -24,9 +24,9 @@ check_amazon() {
             if [[ $status != "Currently unavailable." && $status != "Derzeit nicht verfügbar." ]]; then
                 echo "Go get your PS5 here: https://www.amazon.$region/dp/$version" \
                 | mail -s "PS5 is in stock!" shehzaadsaifulla@gmail.com
-		        echo "$(date):Amazon ""$region"":""$model"":AVAILABLE!!!" >> /opt/ps5.log
+		        echo "$(TZ="Europe/Stockholm" date):Amazon ""$region"":""$model"":AVAILABLE!!!" >> /opt/ps5.log
             else
-                echo "$(date):Amazon ""$region"":""$model"":""$status""" >> /opt/ps5.log
+                echo "$(TZ="Europe/Stockholm" date):Amazon ""$region"":""$model"":""$status""" >> /opt/ps5.log
             fi
             sleep 10
         done
